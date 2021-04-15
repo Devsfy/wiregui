861d0f0e9353882aef053dda6b05182f7fddaac9
Lucas Godoy <lcsdegodoy@outlook.com>
2021-04-14 21:14:07 -0300

    refactor: move check wg installed to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { checkWgIsInstalled } from 'wireguard-tools';
 
 import AppProvider from "./context";
 
 import Home from "./pages/Home";
 
 function App() {
+  // Check if wireguard is installed, since there's nothing
+  // the user can do without it we should just exit the
+  // application after the alert is closed.
+  useEffect(() => {
+    async function check() {
+      try {
+        await checkWgIsInstalled();
+      } catch (err) {
+        alert("Wireguard is not installed on the system.");
+      }
+    }
+
+    check();
+  }, []);
+
   return (
     <AppProvider>
       <Home />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,10 @@
-import React, { useEffect } from "react";
-import { checkWgIsInstalled } from 'wireguard-tools';
+import React from "react";
 
 import { Text } from "@chakra-ui/layout";
 
 import Content from "../components/Content";
 
 export default function Home() {
-
-  // Check if wireguard is installed, since there's nothing
-  // the user can do without it we should just exit the
-  // application after the alert is closed.
-  useEffect(() => {
-    async function check() {
-      try {
-        await checkWgIsInstalled();
-      } catch (err) {
-        alert("Wireguard is not installed on the system.");
-      }
-    }
-
-    check();
-  }, []);
-
   return (
     <Content>
       <Text>Home</Text>
