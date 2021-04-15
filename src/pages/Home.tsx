70f461b5be6f998387df46ecc7e6dcfb3fd9ff1e
Lucas Godoy <lcsdegodoy@outlook.com>
2021-04-14 21:10:00 -0300

    feat: add alert if wireguard is not installed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { checkWgIsInstalled } from 'wireguard-tools';
 
 import { Text } from "@chakra-ui/layout";
 
 import Content from "../components/Content";
 
 export default function Home() {
+
+  // Check if wireguard is installed, since there's nothing
+  // the user can do without it we should just exit the
+  // application after the alert is closed.
+  useEffect(() => {
+    async function check() {
+      try {
+        await checkWgIsInstalled();
+      } catch (err) {
+        console.log(err);
+        alert("Wireguard is not installed on the system.");
+      }
+    }
+
+    check();
+  }, []);
+
   return (
     <Content>
       <Text>Home</Text>
