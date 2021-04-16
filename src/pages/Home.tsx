554e5f8882cccaad8b3b7346ed06c8517071f5ee
Lucas Godoy <lcsdegodoy@outlook.com>
2021-04-16 19:11:39 -0300

    feat: add tips in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,35 @@
 import React from "react";
 
-import { Text } from "@chakra-ui/react";
+import { Flex, Badge, Center } from "@chakra-ui/react";
+import { StarIcon } from "@chakra-ui/icons";
 
 import Content from "../components/Content";
 
 export default function Home() {
   return (
     <Content>
-      <Text>Home</Text>
+      <Flex
+        marginLeft="25px"
+        position="absolute"
+        top="25%"
+        left="50%"
+        direction="column"
+        justify="center"
+      >
+        <Center>
+          <StarIcon w="50%" h="50%" opacity="25%" />
+        </Center>
+        <Center mt="10" opacity="75%">
+          Click{" "}
+          <Badge mx="1" variant="outline" colorScheme="orange">
+            New Connection
+          </Badge>{" "}
+          to add a new connection
+        </Center>
+        <Center mt="4" opacity="75%">
+          Click an existing connection to toggle
+        </Center>
+      </Flex>
     </Content>
   );
 }
