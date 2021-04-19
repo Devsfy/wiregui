d20810ef63c8f779c00817666789096de268c61c
Lucas Godoy <lcsdegodoy@outlook.com>
2021-04-19 10:05:09 -0300

    feat: add auto-updater

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { getIconsPath } from "./utils";
 import "./ipc/main";
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 
+// Invoke the updater.
+require("update-electron-app")();// eslint-disable-line @typescript-eslint/no-var-requires
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) { // eslint-disable-line global-require
   app.quit();
