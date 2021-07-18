e4262a3ba4c3cf836b77d30e852d5290b96e27d5
Lucas Godoy <lcsdegodoy@outlook.com>
2021-07-17 23:23:44 -0300

    feat: add context menu to sidebar items

--- src/components/Sidebar/Sidebartem.tsx
+++ src/components/Sidebar/Sidebartem.tsx
@@ -4,6 +4,7 @@ import { formatDistance } from "date-fns";
 import { Flex, Box, Text } from "@chakra-ui/react";
 
 import { WgConfigFile } from "../../types/store";
+import { ContextMenuTrigger } from "../ContextMenu";
 
 interface SideBarParams extends WgConfigFile {
   /**
@@ -20,36 +21,49 @@ export default function SidebarItem({
   selected,
 }: SideBarParams) {
   return (
-    <Flex
-      bg={selected ? "whiteAlpha.100" : ""}
-      align="center"
-      cursor="pointer"
-      w="100%"
-      p="4"
-      transition=".3s ease"
-      _hover={{ background: "whiteAlpha.200" }}
+    <ContextMenuTrigger
+      menuId={`menu-${name}`}
+      passData={{
+        name,
+      }}
     >
-      <Box
-        bg={active ? "green.400" : "whiteAlpha.600"}
-        w="10px"
-        h="10px"
-        borderRadius="50%"
-      />
-      <Flex direction="column" ml="2" w="89%">
-        <Text
-          color="whiteAlpha.700"
-          w="100%"
-          whiteSpace="nowrap"
-          overflow="hidden"
-          textOverflow="ellipsis"
-        >
-          {name}
-        </Text>
-        <Flex color="whiteAlpha.600" justify="space-between">
-          {address && <Text fontSize="xs">{address[0]}</Text>}
-          <Text fontSize="xs">{lastConnectAt ? formatDistance(new Date(lastConnectAt), new Date(), { addSuffix: true }) : "never"}</Text>
+      <Flex
+        bg={selected ? "whiteAlpha.100" : ""}
+        align="center"
+        cursor="pointer"
+        w="100%"
+        p="4"
+        transition=".3s ease"
+        _hover={{ background: "whiteAlpha.200" }}
+      >
+        <Box
+          bg={active ? "green.400" : "whiteAlpha.600"}
+          w="10px"
+          h="10px"
+          borderRadius="50%"
+        />
+        <Flex direction="column" ml="2" w="89%">
+          <Text
+            color="whiteAlpha.700"
+            w="100%"
+            whiteSpace="nowrap"
+            overflow="hidden"
+            textOverflow="ellipsis"
+          >
+            {name}
+          </Text>
+          <Flex color="whiteAlpha.600" justify="space-between">
+            {address && <Text fontSize="xs">{address[0]}</Text>}
+            <Text fontSize="xs">
+              {lastConnectAt
+                ? formatDistance(new Date(lastConnectAt), new Date(), {
+                    addSuffix: true,
+                  })
+                : "never"}
+            </Text>
+          </Flex>
         </Flex>
       </Flex>
-    </Flex>
+    </ContextMenuTrigger>
   );
 }
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { useHistory, useLocation  } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { Container, Box, Link, Flex, Text } from "@chakra-ui/react";
+
+import * as fs from "fs";
+import * as path from "path";
+import { WgConfig } from "wireguard-tools";
+
 import { version } from "../../../package.json";
-
-import { Box, Link, Flex, Text } from "@chakra-ui/react";
-
-import { StoreState, WgConfigState } from "../../types/store";
+import { ContextMenuItem, ContextMenuList } from "../ContextMenu";
+import { AppState, StoreState, WgConfigState } from "../../types/store";
+import { deleteFile } from "../../store/modules/wgConfig/action";
 
 import NewTunnelButton from "./NewTunnelButton";
 import SidebarItem from "./Sidebartem";
@@ -13,8 +19,12 @@ import SidebarItem from "./Sidebartem";
 export default function Sidebar() {
   const history = useHistory();
   const location = useLocation();
+  const dispatch = useDispatch();
   const { files } = useSelector<StoreState, WgConfigState>(
     (state) => state.wgConfig
+  );
+  const { userDataPath } = useSelector<StoreState, AppState>(
+    (state) => state.app
   );
 
   function handleRedirect(param: string): void {
@@ -29,6 +39,30 @@ export default function Sidebar() {
     return location.pathname.split("/tunnel/")[1] === name;
   }
 
+  function handleDelete(name: string) {
+    try {
+      const filePath = path.join(userDataPath, "configurations", `${name}.conf`);
+
+      const data = fs.readFileSync(filePath, "utf-8");
+      const config = new WgConfig({});
+      config.parse(data);
+
+      const wgConfigFile = files.find((f) => f.name === name);
+      if (!wgConfigFile) {
+        toast("Could not find config file", { type: "error" });
+        return;
+      }
+
+      dispatch(deleteFile(wgConfigFile, userDataPath));
+
+      if (isSelected(name)) {
+        history.push("/");
+      }
+    } catch (e) {
+      toast(e.message, { type: "error" });
+    }
+  }
+
   return (
     <Flex bg="gray.200" direction="column" w="350px" position="relative">
       <Box px="4" pt="4" w="100%">
@@ -41,20 +75,29 @@ export default function Sidebar() {
       </Box>
       <Flex direction="column">
         {files.map((file) => (
-          <Link
-            key={file.name}
-            onClick={() => handleRedirect(file.name)}
-            _hover={{ textDecoration: "none" }}
-          >
-            <SidebarItem
-              name={file.name}
-              path={file.path}
-              address={file.address}
-              lastConnectAt={file.lastConnectAt}
-              active={file.active}
-              selected={isSelected(file.name)}
-            />
-          </Link>
+          <Container key={file.name}>
+            <Link
+              onClick={() => handleRedirect(file.name)}
+              _hover={{ textDecoration: "none" }}
+            >
+              <SidebarItem
+                name={file.name}
+                path={file.path}
+                address={file.address}
+                lastConnectAt={file.lastConnectAt}
+                active={file.active}
+                selected={isSelected(file.name)}
+              />
+            </Link>
+            <ContextMenuList menuId={`menu-${file.name}`}>
+              <ContextMenuItem
+                color="red"
+                onClick={({ passData }) => handleDelete(passData.name as string)}
+              >
+                Delete
+              </ContextMenuItem>
+            </ContextMenuList>
+          </Container>
         ))}
       </Flex>
       <Text
